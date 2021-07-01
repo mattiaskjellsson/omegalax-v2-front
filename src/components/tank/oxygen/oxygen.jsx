930f02f0e466847a5315cd15f31e62719f323bc4
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-07-01 13:12:03 +0800

    Change default gauge max value

--- src/components/tank/oxygen/oxygen.jsx
+++ src/components/tank/oxygen/oxygen.jsx
@@ -22,7 +22,7 @@ export function Oxygen({oxygen, limits}) {
             segmentColors={['#e63535', '#e6ae35', '#e0e635', '#64e635', '#e0e635', '#e6ae35', '#e63535']}
             maxSegmentLabels={5}
             minValue={limit?.oxygenLow ?? 0} 
-            maxValue={limit?.oxygenHigh ?? 100} 
+            maxValue={limit?.oxygenHigh ?? 200} 
             value={oxygen ?? 0} 
             needleTransitionDuration={1000}
           />
