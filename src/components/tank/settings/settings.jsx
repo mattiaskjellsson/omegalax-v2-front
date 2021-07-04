5f9abeabfa629f243a29b74e42efe303144ed286
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-07-04 19:08:05 +0800

    styling for loading and adjusting alarm on/off checkbox a little

--- src/components/tank/settings/settings.jsx
+++ src/components/tank/settings/settings.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from "react-hook-form";
 
 import './settings.css';
 
 export function Settings({limits, onSave}) {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  
+  const [isLoading, setIsLoading] = useState(false)
   const mySubmit = (e) => {
     e.preventDefault();
     handleSubmit(onSave)(e)
+    setIsLoading(true)
   }
+
+  useEffect(() => {
+    setIsLoading(true)
+  }, [limits])
 
   return (
     <div>
+    <div className='overlay' style={ isLoading ? {display: 'none'} : {}}></div>
     <form onSubmit={mySubmit}>
       <div className='f c mt-8'>
         <label className='input-label' htmlFor='oxygenLow'>Low oxygen limit</label>
@@ -26,7 +32,7 @@ export function Settings({limits, onSave}) {
         {errors.oxygenHigh && <div className='error-message'>This field is required</div>}
       </div>
 
-      <div className='f r mt-8'>
+      <div className='f r m mt-8 mb-8'>
         <label className='input-label' htmlFor='isAlarmOn'>Alarm on</label>
         <input defaultChecked={limits?.isAlarmOn} {...register('isAlarmOn')} type='checkbox' />
       </div>
