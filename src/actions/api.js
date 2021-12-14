bd0b9a039a23179205f70142a7ff519ae2549827
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-12-14 08:19:34 +0800

    change production api url

--- src/actions/api.js
+++ src/actions/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import config from './config';
 
 const transport = axios.create({
-  baseURL: `${config.API_PROTOCOL}://${config.API_HOST}:${config.API_PORT}/${config.API_PATH}`,
+  baseURL: `${config.API_PROTOCOL}://${config.API_HOST}:${config.API_PORT}/`,
 })
 
 export const getAllLimits = async () => {
